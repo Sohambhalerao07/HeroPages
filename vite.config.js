a3ceb2ec1f92e5f1ceb0036e343d61add788ac1e
Soham bhalerao <bhaleraosoham07@gmail.com>
2024-11-01 15:14:04 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Hero-Section/' // Correct: leading slash and trailing slash
 })
