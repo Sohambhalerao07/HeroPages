305379cbd8f03896fbf840776888540be4f2aa73
Soham bhalerao <bhaleraosoham07@gmail.com>
2024-11-02 10:31:59 +0530

    Save changes to package.json and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/HeroSection/',
-  build: {
-    outDir: 'dist', // Ensure this matches your deployment setup
-  },
+
 });
